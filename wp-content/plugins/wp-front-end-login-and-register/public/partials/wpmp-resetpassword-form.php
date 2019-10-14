0d32d0b2df881f261efca3d61b8d6b23f5837ca4
Haresh Valiya <haresh.valiya@aloq.se>
2019-10-14 14:55:57 +0530

    login changes

--- wp-content/plugins/wp-front-end-login-and-register/public/partials/wpmp-resetpassword-form.php
+++ wp-content/plugins/wp-front-end-login-and-register/public/partials/wpmp-resetpassword-form.php
@@ -45,7 +45,64 @@ $is_url_has_token ='';
             ?>
             <div class="form-group row">
                 <div class="col-sm-12"><span class="fm-icn eml-icn"></span><input class="form-control" name="wpmp_rp_email" id="wpmp_rp_email" type="Email" placeholder="Email"></div>
-                <div class="or-dvd-line phone-or-email"><span>or</span></div>
+            </div>
+            <input type="hidden" name="wpmp_current_url" id="wpmp_current_url" value="<?php echo get_permalink(); ?>" />
+            <?php
+        else:
+
+            ?>
+            <div class="form-group row">
+                <div class="col-sm-12"><span class="fm-icn eml-icn"></span><input class="form-control" name="wpmp_newpassword" id="wpmp_newpassword" type="password" placeholder="New Password"></div>               
+            </div>
+            <input type="hidden" name="wpmp_rp_email" id="wpmp_rp_email" value="<?php echo $_GET['email'] ?>" />
+            <input type="hidden" name="wpmp_reset_password_token" id="wpmp_reset_password_token" value="<?php echo $_GET['wpmp_reset_password_token']; ?>" />
+
+        <?php
+        endif;
+
+        ?>
+        <?php
+        // this prevent automated script for unwanted spam
+        if (function_exists('wp_nonce_field'))
+            wp_nonce_field('wpmp_resetpassword_action', 'wpmp_resetpassword_nonce');
+
+        ?>
+        <div class="buttonwpr centerbtn"><button type="submit" class="btn defulat-custom-btn"><?php _e($resetpassword_button_text, $this->plugin_name); ?></button></div>
+        <div class="or-dvd-line phone-or-reset"><span>or</span></div>
+        <div class="buttonwpr center">
+            <a href="#" class="btn quirk-btn defulat-custom-btn quirk-resetbtn btnQuirktasticReset">Quirktastic</a>
+        <button type="button" id="btnReturnToLogin" class="btn defulat-custom-btn btnReturnToLogin"><?php _e($returntologin_button_text, $this->plugin_name); ?></button>
+        </div>
+
+    </form>
+</div>
+<div id="wpmpQuirktasticResetPasswordSection" class="hidden <?php //echo empty($is_url_has_token) ? ' hidden' : 'ds' ?>">
+    <?php
+    $wpmp_form_settings = get_option('wpmp_form_settings');
+
+    $resetpassword_form_heading = empty($wpmp_form_settings['wpmp_resetpassword_heading']) ? 'Reset Password' : $wpmp_form_settings['wpmp_resetpassword_heading'];
+    $resetpassword_button_text = empty($wpmp_form_settings['wpmp_resetpassword_button_text']) ? 'Reset password' : $wpmp_form_settings['wpmp_resetpassword_button_text'];
+    $returntologin_button_text = empty($wpmp_form_settings['wpmp_returntologin_button_text']) ? 'Return to Login' : $wpmp_form_settings['wpmp_returntologin_button_text'];
+
+    ?>
+    <div class="signin-title">
+        <h5><?php _e($resetpassword_form_heading, $this->plugin_name); ?></h5>
+    </div>
+
+
+    <div id="wpmp-resetpassword-quirk-loader-info" class="wpmp-loader" style="display:none;">
+        <img src="<?php echo plugins_url('images/ajax-loader.gif', dirname(__FILE__)); ?>"/>
+        <span><?php _e('Please wait ...', $this->plugin_name); ?></span>
+    </div>
+    <div id="wpmp-resetpassword-quirk-alert" class="alert alert-danger" role="alert" style="display:none;"></div>
+
+    <form name="wpmpQuirktasticResetPasswordForm" id="wpmpQuirktasticResetPasswordForm" method="post">
+        <?php
+        // check if the url has token
+        if (!$is_url_has_token) :
+
+            ?>
+            <div class="form-group row">
                 <div class="col-sm-12 email-or-phone">
                     <div class="row">
                         <div class="col-sm-3">
@@ -75,12 +132,12 @@ $is_url_has_token ='';
                 </div>
             </div>
             <input type="hidden" name="wpmp_current_url" id="wpmp_current_url" value="<?php echo get_permalink(); ?>" />
-            <?php
+        <?php
         else:
 
             ?>
             <div class="form-group row">
-                <div class="col-sm-12"><span class="fm-icn eml-icn"></span><input class="form-control" name="wpmp_newpassword" id="wpmp_newpassword" type="password" placeholder="New Password"></div>               
+                <div class="col-sm-12"><span class="fm-icn eml-icn"></span><input class="form-control" name="wpmp_newpassword" id="wpmp_newpassword" type="password" placeholder="New Password"></div>
             </div>
             <input type="hidden" name="wpmp_rp_email" id="wpmp_rp_email" value="<?php echo $_GET['email'] ?>" />
             <input type="hidden" name="wpmp_reset_password_token" id="wpmp_reset_password_token" value="<?php echo $_GET['wpmp_reset_password_token']; ?>" />
@@ -95,9 +152,11 @@ $is_url_has_token ='';
             wp_nonce_field('wpmp_resetpassword_action', 'wpmp_resetpassword_nonce');
 
         ?>
+        <div class="buttonwpr centerbtn"><button type="submit" class="btn defulat-custom-btn"><?php _e($resetpassword_button_text, $this->plugin_name); ?></button></div>
+        <div class="or-dvd-line phone-or-reset"><span>or</span></div>
         <div class="buttonwpr center">
-           <button type="submit" class="btn defulat-custom-btn"><?php _e($resetpassword_button_text, $this->plugin_name); ?></button>
-        <button type="button" id="btnReturnToLogin" class="btn defulat-custom-btn"><?php _e($returntologin_button_text, $this->plugin_name); ?></button>
+            <a href="#" class="btn quirk-btn defulat-custom-btn quirk-resetbtn btnEmailReset">Reset With Email</a>
+            <button type="button" id="btnReturnToLogin" class="btn defulat-custom-btn btnReturnToLogin"><?php _e($returntologin_button_text, $this->plugin_name); ?></button>
         </div>
 
     </form>
