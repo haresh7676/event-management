075cf7386d9eee913b60a690e37f28a7a723b50c
jayeshbhuva <jayesh.bhuva@topsinfosolutions.com>
2019-10-16 19:23:07 +0530

    calendar design

--- wp-content/themes/connie/wp-event-manager/content-single-event_listing.php
+++ wp-content/themes/connie/wp-event-manager/content-single-event_listing.php
@@ -260,7 +260,7 @@ $themesettings =  get_fields('theme-settings');
                         }
                         ?>
 
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/calendar.png" class="img-fluid" alt="">
+                        <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/calendar.png" class="img-fluid" alt=""> -->
                         <?php
                         $startdate = date_i18n( 'Y-m-d', strtotime(get_event_start_date()));
                         $enddate = date_i18n( 'Y-m-d', strtotime(get_event_end_date()) );
